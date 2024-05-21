4f636ef8b747847039f3e09bd7254b596140bee1
Xoj1akbarr <xojiakbarnegmatov7@gmail.com>
2024-05-21 14:56:57 +0500

    git push

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Project</a>
+      <a href="">Service</a>
+    </div>
+  );
+}
+
+export default Navbar;
